abd321c2728ad9012833c763b76d1f8faca2f1ea
Jayaraman Venkatesan <jayvenkat1998@gmail.com>
2023-03-27 22:32:47 +0530

    feat: update CV

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,10 +5,6 @@ export default function Document() {
     <Html lang="en">
 
       <Head>
-        <title>Silvia Prina</title>
-        <meta name="description" content="Silvia Prina Portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </Head>
       <body>
