440472e981ff2cd49dfcc5026db712c81aece052
Jayaraman Venkatesan <jayvenkat1998@gmail.com>
2023-03-28 22:49:55 +0530

    chore: add papers from dropbox

--- src/data/research.config.js
+++ src/data/research.config.js
@@ -4,12 +4,12 @@ export default {
             "heading": "FINANCIAL ACCESS",
             "contents": [
                 {
-                    "title": "Too Fast Too Furious? Digital Credit Speed and Repayments Rates",
+                    "title": "[Too Fast Too Furious? Digital Credit Speed and Repayments Rates](/files/ungated-papers/working-papers/BKP_2022.pdf)",
                     "author": "with Alfredo Burlando and Michael Khun",
                     "journal": "submitted to *The Economic Journal*"
                 },
                 {
-                    "title": "Can A Budget Recording Tool Teach Financial Skills to Youth? Experimental Evidence from a Financial Diaries Study",
+                    "title": "[Can A Budget Recording Tool Teach Financial Skills to Youth? Experimental Evidence from a Financial Diaries Study](/files/ungated-papers/working-papers/FHP_2022.pdf)",
                     "author": "with Veronica Frisancho and Alejandro Herrera",
                     "journal": "revise and resubmit *Journal of Economic Behavior and Organization*"
                 },
@@ -19,17 +19,17 @@ export default {
                     "journal": "conditional acceptance, *Oxford Review of Applied Economics*"
                 },
                 {
-                    "title": "[Migration, Money Transfers and Mobile Money: Evidence from Niger](https://www.aeaweb.org/articles?id=10.1257/pandp.20201085)",
+                    "title": "[Migration, Money Transfers and Mobile Money: Evidence from Niger](/files/ungated-papers/publications/APW_AER_P&P_2020.pdf)",
                     "author": "with Jenny Aker and Jamilah Welch",
                     "journal": "*American Economic Review, Papers & Proceedings*, 2020, 110: 589-93 (Ungated version)"
                 },
                 {
-                    "title": "[CCTs and Financial Access: Increasing the Bang for Each Transferred Buck?](https://doi.org/10.1111/dpr.12199)",
+                    "title": "[CCTs and Financial Access: Increasing the Bang for Each Transferred Buck?](/files/ungated-papers/publications/CP_DPR_2017.pdf)",
                     "author": "with Carlos Chiapa",
                     "journal": "*Development Policy Review*, 2017, 35(1): 23-38 (Ungated version)"
                 },
                 {
-                    "title": "[The Effects of Financial Inclusion on Children's Schooling, and Parental Aspirations and Expectations](https://doi.org/10.1002/jid.3137)",
+                    "title": "[The Effects of Financial Inclusion on Children's Schooling, and Parental Aspirations and Expectations](/files/ungated-papers/publications/CPP_JID_2016.pdf)",
                     "author": "with Carlos Chiapa and Adam Parker",
                     "journal": "*Journal of International Development*, 2016, 28(5): 683-696 (Ungated version)"
                 },
@@ -43,7 +43,7 @@ export default {
                     "journal": "*IPC-UNDP* One Pager, 2015, 292 ISSN: 2318-9118"
                 },
                 {
-                    "title": "[Banking the Poor via Savings Accounts: Evidence from a Field Experiment](https://doi.org/10.1016/j.jdeveco.2015.01.004)",
+                    "title": "[Banking the Poor via Savings Accounts: Evidence from a Field Experiment](/files/ungated-papers/publications/P_JDE_2015.pdf)",
                     "journal": "*Journal of Development Economics*, 2015, 115: 16-31 (Ungated version)"
                 },
             ],
@@ -52,22 +52,22 @@ export default {
             "heading": "NETWORKS AND SPILLOVER EFFECTS",
             "contents": [
                 {
-                    "title": "[Saving Accounts' Interplay with Network-based Financial Arrangements: Evidence from a Field Experiment](https://doi.org/10.1093/ej/ueac053)",
+                    "title": "[Saving Accounts' Interplay with Network-based Financial Arrangements: Evidence from a Field Experiment](/files/ungated-papers/publications/CP_EconomicJournal_2023.pdf)",
                     "author": "with Margherita Comola",
                     "journal": "*The Economic Journal*, 2023, 133 (649): 516-535 (Ungated version)"
                 },
                 {
-                    "title": "[Treatment Effect Accounting for Network Changes](https://doi.org/10.1162/rest_a_00908)",
+                    "title": "[Treatment Effect Accounting for Network Changes](/files/ungated-papers/publications/CP_REStat_2021.pdf)",
                     "author": "with Margherita Comola",
                     "journal": "*Review of Economics and Statistics*, 2021, 103 (3): 597-604 (Ungated version)"
                 },
                 {
-                    "title": "[When Incentives Backfire: Spillover Effects in Food Choice](https://www.aeaweb.org/articles?id=10.1257/pol.20170588)",
+                    "title": "[When Incentives Backfire: Spillover Effects in Food Choice](/files/ungated-papers/publications/APRS_AEJ-Policy_2019.pdf)",
                     "author": "with Manuela Angelucci, Heather Royer, and Anya Savikhin",
                     "journal": "*American Economic Journal: Economic Policy*, 2019, 11(4): 66-95 (Ungated version)"
                 },
                 {
-                    "title": "Adaptation of Peer Messaging from Tuberculosis in India to COVID-19 in Zambia",
+                    "title": "[Adaptation of Peer Messaging from Tuberculosis in India to COVID-19 in Zambia](/files/ungated-papers/working-papers/BCGGHKMP_2022.pdf)",
                     "author": "with Alfredo Burlando, Pradeep Chintagunta, Melissa Graboyes, Jessica Goldberg, Dean Karlan, and Mario Macis",
                     "journal": "submitted"
                 }
@@ -77,12 +77,12 @@ export default {
             "heading": "RACIAL BIAS AND SOCIAL NORMS",
             "contents": [
                 {
-                    "title": "Public Opinion, Racial Bias, and Labor Market Outcomes",
+                    "title": "[Public Opinion, Racial Bias, and Labor Market Outcomes](/files/ungated-papers/working-papers/MPS_2023.pdf)",
                     "author": "with Kaveh Majlesi and Paul Sullivan",
                     "journal": "revise and resubmit *Nature Human Behavior*"
                 },
                 {
-                    "title": "[The Importance of Parental Knowledge and Social Norms: Evidence from Weight Report Cards in Mexico](https://doi.org/10.1016/j.jhealeco.2014.07.001)",
+                    "title": "[The Importance of Parental Knowledge and Social Norms: Evidence from Weight Report Cards in Mexico](/files/ungated-papers/publications/PR_JHE_2014.pdf)",
                     "author": "with Heather Royer",
                     "journal": "*Journal of Health Economics*, 2014, 37: 232-247 (Ungated version)"
                 }
@@ -92,7 +92,7 @@ export default {
             "heading": "DETERMINANTS OF HEALTH AND HUMAN CAPITAL",
             "contents": [
                 {
-                    "title": "C-Sections, Obesity, and Health-Care Specialization: Evidence from Mexico",
+                    "title": "[C-Sections, Obesity, and Health-Care Specialization: Evidence from Mexico](/files/ungated-papers/working-papers/HMP_2021.pdf)",
                     "author": "with Catalina Herrera-Almanza and Fernanda Marquez-Padilla",
                     "journal": "revise and resubmit *Nature Human Behavior*"
                 },
@@ -122,17 +122,17 @@ export default {
             "heading": "INCOME SHOCKS AND CONSUMPTION SMOOTHING",
             "contents": [
                 {
-                    "title": "Transitory Income Changes and Consumption Smoothing: Evidence from Mexico",
+                    "title": "[Transitory Income Changes and Consumption Smoothing: Evidence from Mexico](/files/ungated-papers/working-papers/ACPR_2023.pdf)",
                     "author": "with Manuela Angelucci, Carlos Chiapa and Irvin Rojas",
                     "journal": "revise and resubmit *Journal of Public Economics*"
                 },
                 {
-                    "title": "Shutdown, Irregular SNAP Disbursements and Household Spending",
+                    "title": "[Shutdown, Irregular SNAP Disbursements and Household Spending](/files/ungated-papers/working-papers/MPG_2022.pdf)",
                     "author": "with Mindy Mark and Roy Gernhardt",
                     "journal": "submitted"
                 },
                 {
-                    "title": "[Social Distancing, Stimulus Payments, and Domestic Violence: Evidence from the US during COVID-19](https://www.aeaweb.org/articles?id=10.1257/pandp.20221011)",
+                    "title": "[Social Distancing, Stimulus Payments, and Domestic Violence: Evidence from the US during COVID-19](/files/ungated-papers/publications/BPK_AER_P&P_2022.pdf)",
                     "author": "with Heather Royer",
                     "journal": "*Journal of Health Economics*, 2014, 37: 232-247 (Ungated version)"
                 }
@@ -167,11 +167,11 @@ export default {
             "heading": "OTHER PAPERS",
             "contents": [
                 {
-                    "title": "[Effects of Border Price Changes on Agricultural Wages and Employment in Mexico](https://doi.org/10.1002/jid.2814)",
+                    "title": "[Effects of Border Price Changes on Agricultural Wages and Employment in Mexico](/files/ungated-papers/publications/P_JID_2015.pdf)",
                     "author": "with Veronica Frisancho and Alejandro Herrera"
                 },
                 {
-                    "title": "[Who Benefited More from NAFTA: Small or Large Farmers? Evidence from Mexico](https://doi.org/10.1111/rode.12053)",
+                    "title": "[Who Benefited More from NAFTA: Small or Large Farmers? Evidence from Mexico](/files/ungated-papers/publications/P_RDE_2013.pdf)",
                     "journal": "*Review of Development Economics*, 2013, 17(3): 594-608 (Ungated version)"
                 }
             ],
