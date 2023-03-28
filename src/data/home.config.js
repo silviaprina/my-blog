e5388d336e1989dc13bdf4ac34dfe7f992638a7a
Jayaraman Venkatesan <jayvenkat1998@gmail.com>
2023-03-29 00:02:40 +0530

    chore: incorporate suggestions in intro

--- src/data/home.config.js
+++ src/data/home.config.js
@@ -1,16 +1,13 @@
 export default {
     introductionParagraphs: [
-        `Silvia Prina an Associate Professor in the department of Economics. 
-        She is a development economist whose research focuses on understanding the 
-        behavior of the poor for the purpose of uncovering potential strategies to 
-        improve their lives. Her research interests are centered on financial access, 
-        networks and spillover effects. The bulk of her work uses field experiments and rigorous 
-        empirical methods to study the impact of financial access on the welfare and decision making of the poor. 
-        Her research highlights the role of networks and peers in shaping economic decisions and develops new empirical methods in this context.`,
+        `Silvia Prina an Associate Professor in the department of [Economics](https://cssh.northeastern.edu/economics/) at [Northeastern University](https://www.northeastern.edu/).
+        She is a development economist whose research focuses on understanding the behavior of the poor for the purpose of uncovering potential strategies to improve their lives. 
+        Her research interests are centered on financial access, networks and spillover effects. 
+        The bulk of her work uses field experiments and rigorous empirical methods to study the impact of financial access on the welfare and decision making of the poor. Her research highlights the role of networks and peers in shaping economic decisions and develops new empirical methods in this context.`,
 
-        `Professor Prina is co-editor of [Labour Economics](https://eale.nl/labour-economics/about-labour-economics), 
-        a Research Fellow at the Institute for the Study of Labor ([IZA](https://www.iza.org/people/fellows/7131/silvia-prina/)), 
-        a Research Affiliate at Innovations for Poverty Action ([IPA](https://www.poverty-action.org/node/2251)), and a Faculty Scholar at the 
+        `Professor Prina is Co-Editor of [Labour Economics](https://eale.nl/labour-economics/about-labour-economics), 
+        a Research Fellow at the Institute for the Study of Labor [Institute for the Study of Labor (IZA)](https://www.iza.org/people/fellows/7131/silvia-prina/), 
+        a Research Affiliate at Innovations for Poverty Action [Innovations for Poverty Action (IPA)](https://www.poverty-action.org/node/2251), and a Faculty Scholar at the 
         [Institute for Health Equity and Social Justice Research](https://bouve.northeastern.edu/institute-for-health-equity-and-social-justice-research/silvia-prina-bio/). 
         She is the Graduate Program Director of the [PhD in Applied Economics](https://cssh.northeastern.edu/economics/program/phd-in-applied-economics/). 
         She holds a PhD in Economics from Boston University. Prior to joining Northeastern in 2018, 
