0f2f0e3aa4b37ac2075dedead77e8ffdf4de998f
Jayaraman Venkatesan <jayvenkat1998@gmail.com>
2023-03-28 23:50:12 +0530

    chore: keep research tab before teaching

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 
 export const MENU_LIST = [
     { text: "HOME", path: "/" },
-    { text: "TEACHING", path: "/teaching" },
     { text: "RESEARCH", path: "/research" },
+    { text: "TEACHING", path: "/teaching" },
   ];
