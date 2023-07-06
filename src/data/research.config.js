efdd400f8751e268179114b3b9383ba2f3774d04
silviaprina <129222394+silviaprina@users.noreply.github.com>
2023-07-06 10:02:14 -0400

    Update research.config.js

--- src/data/research.config.js
+++ src/data/research.config.js
@@ -132,11 +132,6 @@ export default {
                     "author": "with Bilge Erten and Pinar Keskin",
                     "journal": "*American Economic Review P&P*, 2022, 112: 262-66 ([Ungated version](/files/ungated-papers/publications/BPK_AER_P&P_2022.pdf))"
                 },
-                {
-                    "title": "[Short-term Labor Supply Response to the Timing of Transfer Payments: Evidence from the SNAP Program](/files/ungated-papers/working-papers/MPT_2023_SNAP_and_Labor_Supply.pdf)",
-                    "author": "with Mindy Marks and Redina Tahaj"
-                    "journal": "submitted"
-                },
                   {
                     "title": "[Short-term Labor Supply Response to the Timing of Transfer Payments: Evidence from the SNAP Program](/files/ungated-papers/working-papers/MPT_2023.pdf)",
                     "author": "with Mindy Marks and Redina Tahaj",
